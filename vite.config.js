ec2cfeefc47bc722cac40520f10398423fcd5e5d
github_christiandc19 <christiandc19@hotmail.com>
2024-11-07 08:46:33 -0800

    styling

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/renewal_recovery/',  // Add this line for correct asset path
+  plugins: [react()]
 })
